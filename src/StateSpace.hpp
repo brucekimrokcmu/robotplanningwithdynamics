a1d3b7fbb56cabfa25900416360eb2bbd6914de9
brucekimrokcmu <kwangkyk@andrew.cmu.edu>
2023-04-10 14:39:59 -0400

    as_integrate StateSpace updated

--- src/StateSpace.hpp
+++ src/StateSpace.hpp
@@ -18,12 +18,25 @@ public:
         VehicleState(double x, double y, double theta, double v, double phi) :
         x_(x), y_(y), theta_(theta), v_(v), phi_(phi){}
     };
-
     StateSpace(double maxX, double maxY, double maxHeadingAngle, double maxSpeed, double maxSteering)
         : maxX(maxX), maxY(maxY),maxHeadingAngle(maxHeadingAngle), maxSpeed(maxSpeed), maxSteering(maxSteering){};
-    VehicleState GetRandoState(int r);
-    bool NearTarget(VehicleState s_new, VehicleState s_target);
-    bool IsValidState();
+    
+    /**
+     * TODO: need change when implementing controller
+     * @return random VehicleState
+    */
+    VehicleState getRandomState(){
+ 
+        double x = (double)rand() / RAND_MAX * maxX;
+        double y = (double)rand() / RAND_MAX * maxY;
+        printf("%f, %f\n", x, y);
+        double theta = (double)rand() / RAND_MAX * maxHeadingAngle;
+        double v = (double)rand() / RAND_MAX * maxSpeed;
+        double phi = (double)rand() / RAND_MAX * maxSteering;
+        return VehicleState(x, y, theta, v, phi);
+    };
+    // bool nearTarget(VehicleState s_new, VehicleState s_target);
+    // bool goalState(VehicleState s_new);
     
 
 private:
