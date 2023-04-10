ec9d8810fbd27734b327a8ad96b7c14269003483
LordAndrew312 <yifansu@andrew.cmu.edu>
2023-04-10 17:38:11 -0400

    fix max

--- src/MotionTree.cpp
+++ src/MotionTree.cpp
@@ -18,6 +18,10 @@ std::vector<MotionTree::Node> MotionTree::getPath(Node v_last){
     while(n.id != 0){
         path.push_back(n);
         n = nodes[n.parent];
+        // if(n.parent >= nodes.size()){
+        //     printf("Hree");
+        // }
+        printf("Hree");
     }
     path.push_back(n);
     return path;
