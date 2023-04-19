218d65a0913d1f588145790b229969de1379b3ea
brucekimrokcmu <kwangkyk@andrew.cmu.edu>
2023-04-18 20:48:30 -0400

    Included Update.hpp

---
+++ src/StateSpace.cpp
@@ -0,0 +1,69 @@
+#include <vector>
+#include <limits>
+#include <iostream>
+#include <cmath>
+#include <random>
+class StateSpace{
+
+public:
+    struct VehicleState{
+        double x_;     // x-position
+        double y_;     // y-position
+        double theta_; // orientation angle
+        double v_;     // velocity
+        double phi_;   // steering angle
+
+        VehicleState(){};
+        VehicleState(double x, double y, double theta, double v, double phi)
+            : x_(x),
+              y_(y),
+              theta_(theta),
+              v_(v),
+              phi_(phi)
+              {
+              };
+    };
+
+    StateSpace(double maxX, double maxY, double maxHeadingAngle, double maxSpeed, double maxSteering)
+        :   maxX(maxX), 
+            maxY(maxY),
+            maxHeadingAngle(maxHeadingAngle), 
+            maxSpeed(maxSpeed), 
+            maxSteering(maxSteering)   
+        {
+        };
+    
+    /**
+     * TODO: need change when implementing controller
+     * @return random VehicleState
+    */
+    VehicleState getRandomState()
+    {
+        double x = (double)rand() / RAND_MAX * maxX;
+        double y = (double)rand() / RAND_MAX * maxY;
+        // printf("%f, %f\n", x, y);
+        double theta = (double)rand() / RAND_MAX * maxHeadingAngle;
+        double v = (double)rand() / RAND_MAX * maxSpeed;
+        double phi = (double)rand() / RAND_MAX * maxSteering;
+        return VehicleState(x, y, theta, v, phi);
+    };
+
+    bool nearTarget(VehicleState s_new, VehicleState s_target)
+    {
+        double x_diff = s_new.x_ - s_target.x_;
+        double y_diff = s_new.y_ - s_target.y_;
+        double theta_diff = s_new.theta_ - s_target.theta_;
+        double v_diff = s_new.v_ - s_target.v_;
+        double phi_diff = s_new.phi_ - s_target.phi_;
+        return (x_diff*x_diff + y_diff*y_diff + theta_diff*theta_diff + v_diff*v_diff + phi_diff*phi_diff) < 0.01;
+    }
+    // bool goalState(VehicleState s_new);
+    
+
+private:
+    double maxX; 
+    double maxY; 
+    double maxHeadingAngle; 
+    double maxSpeed; 
+    double maxSteering;
+};
--- src/Update.hpp
+++ src/Update.hpp
@@ -1,10 +1,18 @@
+#pragma once
 #include <cmath>
 #include <vector>
-#include "Update.hpp"
+
 #include "StateSpace.hpp"
 #include "ControlSpace.hpp"
 
 using namespace std;
+
+struct VehicleGeometry 
+{
+    const double length = 0.3; // check URDF
+
+} VehicleGeometry;
+
 
 class Update
 {
@@ -13,53 +21,12 @@ class Update
         Update(){};
         Update(double length, double width, double maxSpeed, double maxAcceleration, double maxSteeringAngle) {};
 
-        StateSpace::VehicleState Update::Dynamics(StateSpace::VehicleState s_curr, ControlSpace::VehicleControl u, double dt)
-        {
-         
-            double x = s_curr.x_;
-            double y = s_curr.y_;
-            double theta = s_curr.theta_;
-            double v = s_curr.v_;
-            double psi = s_curr.phi_;
-            
-            double acc = u.acc;
-            double steering_rate = u.steering_rate;
 
-            double x_dot = v*cos(theta)*cos(psi);
-            double y_dot = v*sin(theta)*cos(psi);
-            double theta_dot = v*sin(psi)/VehicleGeometry.length;
-            double v_dot = acc;
-            double psi_dot = steering_rate;
 
-            StateSpace::VehicleState s_dot = {x_dot, y_dot, theta_dot, v_dot, psi_dot}; // Dynamics         
+        StateSpace::VehicleState Dynamics(StateSpace::VehicleState s_curr, ControlSpace::VehicleControl u, double dt);
 
-            return s_dot;
-        }
-
-        StateSpace::VehicleState Update::Motion(const StateSpace::VehicleState s_curr, const ControlSpace::VehicleControl u, double dt) 
-        {
-            //Using Explicit Midpoint
-            StateSpace::VehicleState s_temp, s_new;
-            StateSpace::VehicleState f1 = Dynamics(s_curr, u, dt);
-
-            s_temp.x_ = s_curr.x_ + 0.5 * dt * f1.x_;
-            s_temp.y_ = s_curr.y_ + 0.5 * dt * f1.y_;
-            s_temp.theta_ = s_curr.theta_ + 0.5 * dt * f1.theta_;
-            s_temp.v_ = s_curr.v_ + 0.5 * dt * f1.v_;
-            s_temp.phi_ = s_curr.phi_ + 0.5 * dt * f1.phi_;
-
-            StateSpace::VehicleState f2 = Dynamics(s_temp, u, dt);
-
-            s_new.x_ = s_curr.x_ + dt * f2.x_;
-            s_new.y_ = s_curr.y_ + dt * f2.y_;
-            s_new.theta_ = s_curr.theta_ + dt * f2.theta_;
-            s_new.v_ = s_curr.v_ + dt * f2.v_;
-            s_new.phi_ = s_curr.phi_ + dt * f2.phi_;
-
-            return s_new;
-
-        }
-
+        StateSpace::VehicleState Motion(const StateSpace::VehicleState s_curr, const ControlSpace::VehicleControl u, double dt); 
+        
 
         // function normalized_rk4_step(xk,uk)
             
