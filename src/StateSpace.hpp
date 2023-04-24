9f8755e7f84604b56e0858c1061f0d98bcad18cf
brucekimrokcmu <kwangkyk@andrew.cmu.edu>
2023-04-24 19:01:56 -0400

    a separate branch for extended goal: vehicle traversing rough terrain

--- src/StateSpace.hpp
+++ src/StateSpace.hpp
@@ -12,16 +12,27 @@ public:
     struct VehicleState{
         double x_;     // x-position
         double y_;     // y-position
+        double z_;     // z-positon
         double theta_; // orientation angle
-        double v_;     // velocity
+        double vx_;    // linear x-velocity
+        double vy_;    // linear y-velocity
+        double vz_;    // linear z-velocity
+        double wx_;    // angular x-velocity 
+        double wy_;    // angular y-velocity
+        double wz_;    // angular z-velocity
         double phi_;   // steering angle
 
         VehicleState(){};
-        VehicleState(double x, double y, double theta, double v, double phi)
+        VehicleState(double x, double y, double theta, double vx, double vy, double vz, double wx, double wy, double wz, double phi)
             : x_(x),
               y_(y),
               theta_(theta),
-              v_(v),
+              vx_(vx),
+              vy_(vy),
+              vz_(vz),
+              wx_(wx),
+              wy_(wy),
+              wz_(wz),
               phi_(phi)
               {
               };
--- src/Update.hpp
+++ src/Update.hpp
@@ -27,4 +27,13 @@ private:
 
 
 
-
+// x_dot = vx_ * cos(theta_) * cos(phi_) - vy_ * sin(theta_) * cos(phi_)
+// y_dot = vx_ * sin(theta_) * cos(phi_) + vy_ * cos(theta_) * cos(phi_)
+// theta_dot = (wx_ * sin(theta_) + wy_ * cos(theta_)) / cos(phi_)
+// vx_dot = (u_f / m - (vz_ * wy_ - vy_ * wz_) / m) - g * sin(theta_)
+// vy_dot = ((u_f * phi_dot) / m + (vz_ * wx_ - vx_ * wz_) / m) * cos(phi_) - g * cos(theta_) * sin(phi_)
+// vz_dot = ((u_f * phi_dot) / m + (vy_ * wx_ - vx_ * wy_) / m) * sin(phi_) + g * cos(theta_) * cos(phi_)
+// wx_dot = ((Iyy_ - Izz_) * wy_ * wz_ + T_phi_) / Ixx_
+// wy_dot = ((Izz_ - Ixx_) * wz_ * wx_ + T_theta_) / Iyy_
+// wz_dot = ((Ixx_ - Iyy_) * wx_ * wy_ + T_psi_) / Izz_
+// phi_dot = phi_dot_ # phi_dot is a control input
